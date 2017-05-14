48ad79146dff78c49673a01137535496ce8e6782
joshgressman <joshgressman@gmail.com>
2017-05-14 09:06:31 -0500

    assed user routes

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -9,7 +9,7 @@ import { User } from './user.model'; //brinngs in user object
 export class AuthService {
   constructor(private http: Http){}
 
-  signUp(user: User){
+  signup(user: User){
     const body = JSON.stringify(user); //this sets up the data being passed
     const headers = new Headers({'Content-Type': 'application/json'}); //informs backend that it's getting JSON data
     return this.http.post('http://localhost:3000/user',body, {headers: headers})  //body containts the data being passed
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { AuthService } from './auth.service';
-
+import { User } from './user.model';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
@@ -13,7 +13,18 @@ export class SignupComponent implements OnInit {
    constructor(private authService: AuthService){}
 
   onSubmit(){
-    
+    const user = new User(
+      this.myForm.value.email,
+      this.myForm.value.password,
+      this.myForm.value.firstName,
+      this.myForm.value.lastName
+      );
+    this.authService.signup(user)
+      .subscribe(
+         data => console.log(data),
+         error => console.log(error)
+      ); //sends data and alows you to listen to the data
+
     this.myForm.reset();
   }
 
