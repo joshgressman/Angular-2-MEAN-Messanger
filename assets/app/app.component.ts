23008008e3bfe7c45e0a9300aab4f9f84a6ae504
joshgressman <joshgressman@gmail.com>
2017-05-17 15:42:09 -0500

    added message module

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -5,8 +5,8 @@ import { MessageService } from './messages/message.service'; //makes the service
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [MessageService] //Service new is global in the app and does not create an instance per component
+    styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
 
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
 import { HttpModule} from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import { MessageComponent} from "./messages/message.component";
-import { MessageListComponent} from "./messages/message.list.component"
-import { MessageInputComponent} from "./messages/message-input.component"
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
 import { routing } from "./routing/app.routing";
@@ -18,13 +13,11 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
 import { ErrorService } from './errors/error.service';
+import { MessageModule } from './messages/message.module';
+
 @NgModule({
     declarations:[
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
         LogoutComponent,
@@ -32,7 +25,7 @@ import { ErrorService } from './errors/error.service';
         SignupComponent,
         ErrorComponent
     ],
-    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule, HttpModule],
+    imports: [BrowserModule, routing, ReactiveFormsModule, HttpModule, MessageModule],
     providers: [AuthService, ErrorService], //Makes accessible across whole app
     bootstrap: [AppComponent]
 })
